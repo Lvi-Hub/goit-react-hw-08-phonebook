14dec7bd91d175615838584045cd8d22b9ec0924
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-07-03 00:25:26 +0200

    add styles

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,15 +1,15 @@
 import css from './AuthNav.module.css';
 import { NavLink } from 'react-router-dom';
 
-export default function AuthNav(){
+export default function AuthNav() {
   return (
     <div>
       <NavLink className={css.navlink} to="/register">
         Register
       </NavLink>
-      <NavLink to="/login">
+      <NavLink className={css.navlink} to="/login">
         Log In
       </NavLink>
     </div>
   );
-};
+}
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -3,38 +3,26 @@ import { contactReducer } from './contacts/contactSlice';
 import { filterReducer } from './filtersSlice';
 import { authReducer } from './auth/slice';
 import storage from 'redux-persist/lib/storage';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 
 const authPersistConfig = {
   key: 'auth',
   storage,
-  whitelist: ['token'],
+  whitelist: ['token', 'user'],
 };
-const persistedReducer = persistReducer(authPersistConfig, authReducer)
+const persistedReducer = persistReducer(authPersistConfig, authReducer);
 export const store = configureStore({
-  reducer: {    
+  reducer: {
     contacts: contactReducer,
     filter: filterReducer,
     auth: persistedReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-  getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
- 
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
-
